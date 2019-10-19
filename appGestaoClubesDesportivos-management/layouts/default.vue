7bf8fa35841ea53d47ac2885139fd5f7b66a5f41
Andreia <andreiasofiasantosrocha@hotmail.com>
2019-10-19 03:45:46 +0100

    Commit da Camada de Aprentação - vue.js

---
+++ appGestaoClubesDesportivos-management/layouts/default.vue
@@ -0,0 +1,8 @@
+// file default.vue --> represents the default app layout, if not specified in a page which layout to use.
+<template>
+  <div>
+    <nuxt /> <!-- mandatory. DO NOT REMOVE -->
+  </div>
+</template>
+<style>
+</style>
---
+++ appGestaoClubesDesportivos-management/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- easy components usage, already shipped with bootstrap css-->
+  <b-container>
+    <!-- try to remove :fields=”fields” to see the magic -->
+    <b-table striped over :items="administradores" :fields="fields" />
+  </b-container>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                fields: ['userId', 'nome', 'password', 'email'],
+                administradores: []
+            }
+        },
+        created () {
+            this.$axios.$get("http://localhost:8080/AppGestaoClubesDesportivos_war_exploded/api/administradores/")
+            this.$axios.$get("/api/administradores")
+                .then(administradores => {
+                    this.administradores = administradores
+                })
+        }
+    }
+</script>
+<style>
+</style>
